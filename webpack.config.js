4fb319137543ad1b7080e3a68dcf067addfd2727
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-13 11:50:33 -0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 8080,
+    open: true,
+    liveReload: true
+  },
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets', to: 'assets' },
+        { from: 'src/styles', to: 'styles' },
+      ]
+    })
+  ]
+}

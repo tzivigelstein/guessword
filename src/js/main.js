3d72f9c29eddec3b2a7e3aa61f34bb2a2c511c5f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-15 13:21:48 -0300

    Add before and after words functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,66 @@
 import utils from './utils/index.js'
 import words from 'an-array-of-spanish-words'
 
-const { $ } = utils
+const { $, $$ } = utils
 
 const wordsList = []
 
 const form = $('.mainContainer')
 
-const wordOfTheDay = 'Increible'
+const wordOfTheDay = 'increible'
+
+const [afterHint, beforeHint] = $$('.hint')
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  const userWord = e.target.elements.word.value
+  const userWord = e.target.elements.word.value.toLowerCase()
 
   if (userWord === '') return
   if (wordsList.includes(userWord)) return
   if (!words.includes(userWord)) return
+  if (userWord === wordOfTheDay) {
+    alert('great')
+    return
+  }
 
   wordsList.push(userWord)
+
+  const [beforeWords, afterWords] = getBeforeAndAfterWords(wordsList)
+
+  const beforeWordsContainer = $('.beforeWordsContainer')
+  const afterWordsContainer = $('.afterWordsContainer')
+
+  if (userWord < wordOfTheDay) {
+    const afterWord = $('#afterWord')
+    afterWord.innerText = afterWords[0]
+
+    afterWordsContainer.innerHTML = ''
+
+    afterWords.length !== 0 &&
+      afterWords.forEach((word, index) => {
+        const wordElement = createWordElement({ word, index })
+        afterWordsContainer.appendChild(wordElement)
+      })
+
+    afterHint.classList.remove('hiddenHint')
+  } else {
+    const beforeWord = $('#beforeWord')
+    beforeWord.innerText = beforeWords[0]
+
+    beforeWordsContainer.innerHTML = ''
+    beforeWords.length !== 0 &&
+      beforeWords.forEach((word, index) => {
+        const wordElement = createWordElement({ word, index })
+        beforeWordsContainer.appendChild(wordElement)
+      })
+
+    beforeHint.classList.remove('hiddenHint')
+  }
+
   e.target.elements.word.value = ''
-  showBeforeAndAfter()
 })
 
-function showBeforeAndAfter() {
+function getBeforeAndAfterWords(wordsList) {
   const sortedWords = wordsList.sort(function (a, b) {
     if (a < b) {
       return -1
@@ -33,5 +71,26 @@ function showBeforeAndAfter() {
     return 0
   })
 
-  console.log(sortedWords)
+  const afterWords = sortedWords
+    .filter(word => word < wordOfTheDay)
+    .sort(function (a, b) {
+      if (a < b) {
+        return 1
+      }
+      if (a > b) {
+        return -1
+      }
+      return 0
+    })
+  const beforeWords = sortedWords.filter(word => word > wordOfTheDay)
+
+  return [beforeWords, afterWords]
+}
+
+function createWordElement({ word, index }) {
+  const wordElement = document.createElement('li')
+  wordElement.classList.add('pastWord')
+  wordElement.style.fontSize = `${1.5 / (index / 10 + 1)}rem`
+  wordElement.innerText = word
+  return wordElement
 }
